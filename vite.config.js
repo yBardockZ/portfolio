ab36ac820d42226999f82dfd7d85d1235364cd32
Thalles <thalles_leopoldino@outlook.com>
2025-11-16 02:48:46 -0300

    Removendo a configuração de minificação do terser do arquivo vite.config.js para compatibilidade com a versão de produção.

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  build: {
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'framer-motion': ['framer-motion'],
-          'react-icons': ['react-icons'],
-        },
-      },
-    },
-  },
   server: {
     port: 5173,
     open: false,
